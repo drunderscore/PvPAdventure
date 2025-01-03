6d36b8b2a2548a5714bb7e6ac60e1c602553e41a
Garmon <Ljsm0007@gmail.com>
2025-01-03 13:56:32 -0600

    Fixed an invalid variable reference (whoops) and deleted some obsolete localization entries.

--- System/RecipeManager.cs
+++ System/RecipeManager.cs
@@ -9,16 +9,16 @@ public class RecipeManager : ModSystem
     {
         for (int i = 0; i < ModContent.GetInstance<AdventureConfig>().DuplicateItemRecipes.Count; i++)
         {
-            for (int result = 0; result < ModContent.GetInstance<AdventureConfig>().DuplicateItemRecipes[i].EnemyDrops.Count; result++)
+            for (int result = 0; result < ModContent.GetInstance<AdventureConfig>().DuplicateItemRecipes[i].Materials.Count; result++)
             {
-                for (int material = 0; material < ModContent.GetInstance<AdventureConfig>().DuplicateItemRecipes[i].EnemyDrops.Count; material++)
+                for (int material = 0; material < ModContent.GetInstance<AdventureConfig>().DuplicateItemRecipes[i].Materials.Count; material++)
                 {
                     Recipe recipe;
                     if (result == material)
                         continue;
 
-                    recipe = Recipe.Create(ModContent.GetInstance<AdventureConfig>().DuplicateItemRecipes[i].EnemyDrops[result].Type)
-                        .AddIngredient(ModContent.GetInstance<AdventureConfig>().DuplicateItemRecipes[i].EnemyDrops[material].Type, ModContent.GetInstance<AdventureConfig>().DuplicateItemRecipes[i].AmountNeeded);
+                    recipe = Recipe.Create(ModContent.GetInstance<AdventureConfig>().DuplicateItemRecipes[i].Materials[result].Type)
+                        .AddIngredient(ModContent.GetInstance<AdventureConfig>().DuplicateItemRecipes[i].Materials[material].Type, ModContent.GetInstance<AdventureConfig>().DuplicateItemRecipes[i].AmountNeeded);
 
                     if (ModContent.GetInstance<AdventureConfig>().DuplicateItemRecipes[i].Workstation.Type != -1)
                         recipe.AddTile(ModContent.GetInstance<AdventureConfig>().DuplicateItemRecipes[i].Workstation.Type);
