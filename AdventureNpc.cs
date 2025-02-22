9d78224cd5652372f93089f09a5423b3daacebda
Garmon <Ljsm0007@gmail.com>
2025-02-22 14:01:52 -0600

    Changed drop rates for various enemies

--- AdventureNpc.cs
+++ AdventureNpc.cs
@@ -171,6 +171,99 @@ public class AdventureNpc : GlobalNPC
     {
         if (IsPartOfEaterOfWorlds((short)npc.type))
             npcLoot.Add(ItemDropRule.ByCondition(new Conditions.LegacyHack_IsBossAndNotExpert(), ItemID.WormScarf));
+
+        if (npc.type == NPCID.KingSlime)
+            npcLoot.Add(ItemDropRule.ByCondition(new Conditions.LegacyHack_IsBossAndNotExpert(), ItemID.RoyalGel));
+
+        if (npc.type == NPCID.EyeofCthulhu)
+            npcLoot.Add(ItemDropRule.ByCondition(new Conditions.LegacyHack_IsBossAndNotExpert(), ItemID.EoCShield));
+
+        if (IsPartOfEaterOfWorlds((short)npc.type) || npc.type == NPCID.BrainofCthulhu)
+            npcLoot.Add(ItemDropRule.ByCondition(new Conditions.LegacyHack_IsBossAndNotExpert(), ItemID.WormScarf));
+
+        if (npc.type == NPCID.QueenBee)
+            npcLoot.Add(ItemDropRule.ByCondition(new Conditions.LegacyHack_IsBossAndNotExpert(), ItemID.HiveBackpack));
+
+        if (npc.type == NPCID.Deerclops)
+            npcLoot.Add(ItemDropRule.ByCondition(new Conditions.LegacyHack_IsBossAndNotExpert(), ItemID.BoneHelm));
+
+        if (npc.type == NPCID.SkeletronHead)
+            npcLoot.Add(ItemDropRule.ByCondition(new Conditions.LegacyHack_IsBossAndNotExpert(), ItemID.BoneGlove));
+
+        if (npc.type == NPCID.QueenSlimeBoss)
+            npcLoot.Add(ItemDropRule.ByCondition(new Conditions.LegacyHack_IsBossAndNotExpert(), ItemID.VolatileGelatin));
+
+        if (npc.type == NPCID.TheDestroyer)
+            npcLoot.Add(ItemDropRule.ByCondition(new Conditions.LegacyHack_IsBossAndNotExpert(), ItemID.MechanicalWagonPiece));
+
+        if (npc.type == NPCID.Retinazer || npc.type == NPCID.Spazmatism)
+            npcLoot.Add(ItemDropRule.ByCondition(new Conditions.LegacyHack_IsBossAndNotExpert(), ItemID.MechanicalWheelPiece));
+
+        if (npc.type == NPCID.SkeletronPrime)
+            npcLoot.Add(ItemDropRule.ByCondition(new Conditions.LegacyHack_IsBossAndNotExpert(), ItemID.MechanicalBatteryPiece));
+
+        if (npc.type == NPCID.Plantera)
+            npcLoot.Add(ItemDropRule.ByCondition(new Conditions.LegacyHack_IsBossAndNotExpert(), ItemID.SporeSac));
+
+        foreach (var rule in npcLoot.Get())
+        {
+            if (rule is DropBasedOnExpertMode drop)
+            {
+                if (drop.ruleForNormalMode is CommonDrop normalDropRule)
+                {
+                    switch (normalDropRule.itemId)
+                    {
+                        case ItemID.Tabi:
+                            normalDropRule.chanceDenominator = 4;
+                            break;
+
+                        case ItemID.PaladinsHammer:
+                        case ItemID.PaladinsShield:
+                            normalDropRule.chanceNumerator = 3;
+                            normalDropRule.chanceDenominator = 20;
+                            break;
+
+                        case ItemID.ShadowbeamStaff:
+                        case ItemID.SpectreStaff:
+                        case ItemID.InfernoFork:
+                        case ItemID.SniperRifle:
+                        case ItemID.TacticalShotgun:
+                        case ItemID.RocketLauncher:
+                            normalDropRule.chanceNumerator = 1;
+                            normalDropRule.chanceDenominator = 10;
+                            break;
+
+                        case ItemID.MaceWhip:
+                            normalDropRule.chanceNumerator = 3;
+                            normalDropRule.chanceDenominator = 400;
+                            break;
+
+                        case ItemID.SpiderFang:
+                            normalDropRule.chanceNumerator = 4;
+                            normalDropRule.chanceDenominator = 5;
+                            break;
+                    }
+                }
+            }
+
+            if (rule is ItemDropWithConditionRule conditionalDrop)
+            {
+                if (conditionalDrop.itemId == ItemID.ButterflyDust)
+                {
+                    conditionalDrop.chanceNumerator = 1;
+                    conditionalDrop.chanceDenominator = 1;
+                }
+            }
+
+            if (rule is CommonDrop commonDrop)
+            {
+                if (commonDrop.itemId == ItemID.MagicQuiver)
+                {
+                    commonDrop.chanceNumerator = 1;
+                    commonDrop.chanceDenominator = 50;
+                }
+            }
+        }
     }
 
     public override void EditSpawnRate(Player player, ref int spawnRate, ref int maxSpawns)
