eb2e8bbc2ecf13d61ced1e971b00b5c8074ec7a1
Alex <87573461+MatteSevai@users.noreply.github.com>
2025-03-24 22:38:28 -0700

    test changes
    really good changes dont look at them though

--- AdventureNpc.cs
+++ AdventureNpc.cs
@@ -19,7 +19,8 @@ public class AdventureNpc : GlobalNPC
 {
     public override bool InstancePerEntity => true;
     public DamageInfo LastDamageFromPlayer { get; set; }
-
+    //test
+    //test
     public class DamageInfo(byte who)
     {
         public byte Who { get; } = who;
