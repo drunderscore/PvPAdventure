f52ab732fc4d7ed9d6d701ebfe945397be7bea22
Alex <87573461+MatteSevai@users.noreply.github.com>
2025-03-30 16:37:11 -0700

    nerfed cursed inferno and venum,

---
+++ DebuffChanges.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Terraria.ModLoader;
+
+namespace PvPAdventure
+{
+    internal class CursedFlameDebuffPlayer : ModPlayer
+    {
+        //Changes dps for varius player debuffs
+        public override void UpdateBadLifeRegen()
+        {
+            if (base.Player.HasBuff(39))
+            {
+                base.Player.lifeRegen += 7;
+                base.Player.lifeRegenTime = 0f;
+            }
+            if (base.Player.HasBuff(70))
+            {
+                base.Player.lifeRegen += 14;
+                base.Player.lifeRegenTime = 0f;
+            }
+        }
+    }
+}
