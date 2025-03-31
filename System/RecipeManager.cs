f6b965384e1a4cb93d11163d03210826acae589b
Alex <87573461+MatteSevai@users.noreply.github.com>
2025-03-30 17:00:56 -0700

    Added EJ CHanges for shimmer and temp replacements crafting

--- System/RecipeManager.cs
+++ System/RecipeManager.cs
@@ -23,12 +23,6 @@ namespace PvPAdventure.System
             [ItemID.Flairon, ItemID.Tsunami, ItemID.RazorbladeTyphoon, ItemID.BubbleGun, ItemID.TempestStaff]
         ];
 
-        public override void AddRecipes()
-        {
-            foreach (var lootTable in _lootTables)
-                CreateDuplicateDropRecipe(lootTable, 3);
-        }
-
         private static void CreateDuplicateDropRecipe(List<int> lootTable, int amountOfMaterial)
         {
             for (int i = 0; i < lootTable.Count; i++)
@@ -44,6 +38,89 @@ namespace PvPAdventure.System
                         .Register();
                 }
             }
+        }
+
+        public override void AddRecipes()
+        {
+            //adds the multi boss drop recipies (placeholder code)
+            foreach (var lootTable in _lootTables)
+                CreateDuplicateDropRecipe(lootTable, 3);
+           
+            // Convert Cursed Flames to Ichor
+            Recipe.Create(ItemID.Ichor)
+                .AddIngredient(ItemID.CursedFlame)
+                .AddTile(476) // Shimmering Pool tile ID
+                .Register();
+
+            // Convert Ichor to Cursed Flames
+            Recipe.Create(ItemID.CursedFlame)
+                .AddIngredient(ItemID.Ichor)
+                .AddTile(476) // Shimmering Pool tile ID
+                .Register();
+
+            Recipe.Create(ItemID.CrystalNinjaChestplate)
+                .AddIngredient(ItemID.CrystalNinjaHelmet)
+                .AddTile(476) // Shimmering Pool tile ID
+                .Register();
+
+            Recipe.Create(ItemID.CrystalNinjaLeggings)
+                .AddIngredient(ItemID.CrystalNinjaChestplate)
+                .AddTile(476) // Shimmering Pool tile ID
+                .Register();
+
+            Recipe.Create(ItemID.CrystalNinjaHelmet)
+                .AddIngredient(ItemID.CrystalNinjaLeggings)
+                .AddTile(476) // Shimmering Pool tile ID
+                .Register();
+
+            Recipe.Create(ItemID.GladiatorBreastplate)
+                .AddIngredient(ItemID.GladiatorHelmet)
+                .AddTile(476) // Shimmering Pool tile ID
+                .Register();
+
+            Recipe.Create(ItemID.GladiatorLeggings)
+                .AddIngredient(ItemID.GladiatorBreastplate)
+                .AddTile(476) // Shimmering Pool tile ID
+                .Register();
+
+            Recipe.Create(ItemID.GladiatorHelmet)
+                .AddIngredient(ItemID.GladiatorLeggings)
+                .AddTile(476) // Shimmering Pool tile ID
+                .Register();
+
+            //placeholder code before proper item replacments
+            Recipe.Create(ItemID.Tabi)
+                .AddIngredient(ItemID.BlackBelt)
+                .Register();
+
+            Recipe.Create(ItemID.AncientChisel)
+                .AddIngredient(ItemID.MagicConch)
+                .Register();
+
+            Recipe.Create(ItemID.XenoStaff)
+                .AddIngredient(ItemID.CosmicCarKey)
+                .Register();
+
+            Recipe.Create(ItemID.CloudinaBottle)
+                .AddIngredient(ItemID.MagicMirror)
+                .Register();
+
+            Recipe.Create(ItemID.FeralClaws)
+                .AddIngredient(ItemID.Seaweed)
+                .Register();
+
+            Recipe.Create(ItemID.BlizzardinaBottle)
+                .AddIngredient(ItemID.Fish)
+                .Register();
+
+            Recipe.Create(ItemID.TacticalShotgun)
+                .AddIngredient(ItemID.Shotgun)
+                .AddIngredient(ItemID.IllegalGunParts, 11)
+                .Register();
+
+            Recipe.Create(ItemID.MagicMirror)
+                .AddIngredient(ItemID.Cloud, 243)
+                .Register();
         }
     }
 }
