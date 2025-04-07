78ba4402839a25f984dc3930868ace17e15fa014
Alex <87573461+MatteSevai@users.noreply.github.com>
2025-04-07 13:18:40 -0700

    Revert "W commit, should have changed the code back to a temporary working position. Still can't see errors"
    This reverts commit f2a216c88e3d156e351fa5094d9a60635519a1a4.

--- ShinyStoneHotswap.cs
+++ ShinyStoneHotswap.cs
@@ -2,10 +2,13 @@
 using Terraria.ID;
 using Terraria.ModLoader;
 
-namespace Tpvpaquickaddon.Content.Buffs
+namespace PvPAdventure
 {
+
     public class ShinyStoneHotswap : ModBuff
+
     {
+        public override string Texture => $"PvPAdventure/Assets/Buff/ShinyStoneHotswap}";
         public override void SetStaticDefaults()
         {
             Main.debuff[Type] = true;
@@ -63,6 +66,24 @@ namespace Tpvpaquickaddon.Content.Buffs
             Main.buffNoSave[Type] = true; 
             Main.buffNoTimeDisplay[Type] = false; 
             Main.buffNoTimeDisplay[Type] = false; // Show timer
+            Main.buffNoSave[Type] = true; 
+            Main.buffNoTimeDisplay[Type] = false; 
         }
     }
-}
+
+
+
+        private bool IsShinyStoneEquipped()
+        {
+            for (int i = 3; i < 10; i++) // Check all accessory slots 1-5
+            {
+                if (Player.armor[i].type == ItemID.ShinyStone &&
+                   (i < 7 || !Player.hideVisibleAccessory[i - 3]))
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+    }
+)
