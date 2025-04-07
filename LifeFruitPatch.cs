05f45c2383bff7f7d207819dc507a60508bedfc5
Alex <87573461+MatteSevai@users.noreply.github.com>
2025-04-07 12:08:32 -0700

    life fuit p[atch doesnt work yet

--- LifeFruitPatch.cs
+++ LifeFruitPatch.cs
@@ -1,12 +1,37 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Terraria;
+using Terraria.ModLoader;
+using MonoMod.Cil;
 
-namespace PvPAdventure
+namespace LifeFruitPatcher
 {
-    class LifeFruitPatch
+    public class LifeFruitPatcherMod : ModSystem
     {
+        public override void Load()
+        {
+            // Hook into the IL of WorldGen.UpdateWorld_GrassGrowth
+            IL_WorldGen.UpdateWorld_GrassGrowth += ModifyGrassGrowthIL;
+        }
+
+        private void ModifyGrassGrowthIL(ILContext il)
+        {
+            var cursor = new ILCursor(il);
+
+            // Patch 1: Change maxValue2 (non-expert) from 40 → 2
+            cursor.GotoNext(MoveType.After,
+                x => x.MatchLdcI4(40));
+                cursor.Prev.Operand = 2;
+            
+
+            
+           
+            cursor.Index = 0;
+
+            // Patch 2: Change num7 (radius) from 60 → 2
+            cursor.TryGotoNext(MoveType.After,
+                x => x.MatchLdcI4(60));
+                cursor.Prev.Operand = 2;
+            
+           
+        }
     }
 }
