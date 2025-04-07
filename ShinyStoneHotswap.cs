82dbfddf1e82a6ae06b64c24168c8ea5ee66d611
EJCrispy <ej@soundstasty.com>
2025-04-06 22:00:34 -0700

    Followed all the instructions to the best of my ability. Hopefully this works but I don't have high hopes not going to lie.

--- ShinyStoneHotswap.cs
+++ ShinyStoneHotswap.cs
@@ -4,49 +4,20 @@ using Terraria.ModLoader;
 
 namespace PvPAdventure
 {
+
     public class ShinyStoneHotswap : ModBuff
+
     {
+        public override string Texture => $"PvPAdventure/Assets/Buff/ShinyStoneHotswap}";
         public override void SetStaticDefaults()
         {
-            Main.debuff[Type] = true; // duh duh
-            Main.buffNoSave[Type] = true; // doesn't matter because of anti-hotswap rule applying upon rejoining
-            Main.buffNoTimeDisplay[Type] = false; // Show timer
+            Main.debuff[Type] = true;
+            Main.buffNoSave[Type] = true; 
+            Main.buffNoTimeDisplay[Type] = false; 
         }
     }
 
-    public class ShinyStoneDisabler : ModPlayer
-    {
-        private bool hadShinyStoneLastFrame;
 
-        public override void PostUpdateEquips()
-        {
-            // Check if Shiny Stone is equipped
-            bool hasShinyStone = IsShinyStoneEquipped();
-
-            // Apply debuff when first equipped or when you relog and rejoin (unintentional but works so who gafs)
-            if (hasShinyStone && !hadShinyStoneLastFrame)
-            {
-                Player.AddBuff(ModContent.BuffType<ShinyStoneHotswap>(), 3600); // 60 seconds of charging upon being swapped on
-            }
-
-            // Disable Shiny Stone effects while debuffed (it just works)
-            if (Player.HasBuff(ModContent.BuffType<ShinyStoneHotswap>()))
-            {
-                Player.shinyStone = false;
-            }
-
-            hadShinyStoneLastFrame = hasShinyStone;
-        }
-
-        public override void OnRespawn()
-        {
-            // We re-apply debuff if equipped and you respawn
-            //TODO: we could maybe make it so that if you equip the shiny stone witin the bounds of the spawnbox you don't get the Charging debuff
-            if (IsShinyStoneEquipped())
-            {
-                Player.AddBuff(ModContent.BuffType<ShinyStoneHotswap>(), 900); // 15 seconds of Charging after respawnin
-            }
-        }
 
         private bool IsShinyStoneEquipped()
         {
@@ -61,4 +32,4 @@ namespace PvPAdventure
             return false;
         }
     }
-}
+)
