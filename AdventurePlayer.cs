de345f027684e782de553fc337a045458c5e966a
Alex <87573461+MatteSevai@users.noreply.github.com>
2025-04-07 13:18:27 -0700

    Revert "W commit, should have changed the code back to a temporary working position. Still can't see errors"
    This reverts commit f2a216c88e3d156e351fa5094d9a60635519a1a4.

--- AdventurePlayer.cs
+++ AdventurePlayer.cs
@@ -792,19 +792,17 @@ public class AdventurePlayer : ModPlayer
         return $"{Player.whoAmI}/{Player.name}/{DiscordUser?.Id}";
     }
     {
- public class ShinyStoneDisabler : ModPlayer
-    {
-        private bool hadShinyStoneLastFrame;
+     private bool _hadShinyStoneLastFrame;
 
         public override void PostUpdateEquips()
         {
             // Check if Shiny Stone is equipped
-            bool hasShinyStone = IsShinyStoneEquipped();
+            var hasShinyStone = IsShinyStoneEquipped();
 
-            // Apply debuff when first equipped or after respawn
+            // Apply debuff when first equipped or when you relog and rejoin (unintentional but works so who gafs)
             if (hasShinyStone && !hadShinyStoneLastFrame)
             {
-                Player.AddBuff(ModContent.BuffType<ShinyStoneHotswap>(), 3600); // 60 seconds
+                Player.AddBuff<ShinyStoneHotswap>, 60 * 60);
             }
 
             // Disable Shiny Stone effects while debuffed
@@ -813,29 +811,15 @@ public class AdventurePlayer : ModPlayer
                 Player.shinyStone = false;
             }
 
-            hadShinyStoneLastFrame = hasShinyStone;
+            _hadShinyStoneLastFrame = hasShinyStone;
         }
-
         public override void OnRespawn()
         {
-            // Re-apply debuff if equipped during respawn
+            // We re-apply debuff if equipped and you respawn
+            //TODO: we could maybe make it so that if you equip the shiny stone witin the bounds of the spawnbox you don't get the Charging debuff
             if (IsShinyStoneEquipped())
             {
-                Player.AddBuff(ModContent.BuffType<ShinyStoneHotswap>(), 900);
+                Player.AddBuff<ShinyStoneHotswap>, 15 * 60);
             }
-        }
-
-        private bool IsShinyStoneEquipped()
-        {
-            for (int i = 3; i < 10; i++) // Check all accessory slots
-            {
-                if (Player.armor[i].type == ItemID.ShinyStone &&
-                   (i < 7 || !Player.hideVisibleAccessory[i - 3]))
-                {
-                    return true;
-                }
-            }
-            return false;
         }
     }
-}
--- ShinyStoneHotswap.cs
+++ ShinyStoneHotswap.cs
@@ -2,10 +2,13 @@
 using Terraria.ID;
 using Terraria.ModLoader;
 
-namespace Tpvpaquickaddon.Content.Buffs
+namespace PvPAdventure
 {
+
     public class ShinyStoneHotswap : ModBuff
+
     {
+        public override string Texture => $"PvPAdventure/Assets/Buff/ShinyStoneHotswap}";
         public override void SetStaticDefaults()
         {
             Main.debuff[Type] = true;
@@ -61,6 +64,25 @@ namespace Tpvpaquickaddon.Content.Buffs
         }
     }
             Main.buffNoTimeDisplay[Type] = false; // Show timer
+            Main.buffNoSave[Type] = true; 
+            Main.buffNoTimeDisplay[Type] = false; 
+
         }
     }
-}
+
+
+
+        private bool IsShinyStoneEquipped()
+        {
+            for (int i = 3; i < 10; i++) // Check all accessory slots 1-5
+            {
+                if (Player.armor[i].type == ItemID.ShinyStone &&
+                   (i < 7 || !Player.hideVisibleAccessory[i - 3]))
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+    }
+)
