4dc2b09bb49eb4e1176b707ecd509759b1cd8a55
EJCrispy <ej@soundstasty.com>
2025-04-07 13:15:12 -0700

    Should just work, I CAN SEE ERRORS NOW!

---
+++ ShinyStoneHotswap.cs
@@ -0,0 +1,14 @@
+﻿using Terraria;
+using Terraria.ModLoader;
+
+namespace PvPAdventure;
+
+public class ShinyStoneHotswap : ModBuff
+{
+    public override void SetStaticDefaults()
+    {
+        Main.debuff[Type] = true;
+        Main.buffNoSave[Type] = true;
+        Main.buffNoTimeDisplay[Type] = false; // Show timer
+    }
+}
