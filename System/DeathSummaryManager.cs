0916c38359c4a3d6fec50c072fd41778e58cf483
James Puleo <james@jame.xyz>
2025-06-08 22:36:55 -0400

    abstraction! Allow any kind of damage

--- System/DeathSummaryManager.cs
+++ System/DeathSummaryManager.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections.Generic;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
@@ -48,6 +49,66 @@ public class DeathSummaryManager : ModSystem
         }
     }
 
+    public interface IDamageSource
+    {
+        public static readonly Color DefaultColor = new(160, 160, 160);
+
+        public string Name { get; }
+        public Color Color => DefaultColor;
+    }
+
+    public class DamageClassDamageSource(DamageClass damageClass) : IDamageSource
+    {
+        private readonly DamageClass _damageClass = damageClass;
+
+        private static readonly Color GenericColor = new(221, 80, 71);
+        private static readonly Color SummonColor = new(79, 203, 203);
+        private static readonly Color MeleeColor = new(219, 220, 49);
+
+        private static readonly Dictionary<DamageClass, Color> Colors = new()
+        {
+            { DamageClass.Default, IDamageSource.DefaultColor },
+            { DamageClass.Generic, GenericColor },
+            { DamageClass.Melee, MeleeColor },
+            { DamageClass.MeleeNoSpeed, MeleeColor },
+            { DamageClass.Ranged, new(21, 194, 86) },
+            { DamageClass.Magic, new(204, 31, 198) },
+            { DamageClass.Summon, SummonColor },
+            { DamageClass.SummonMeleeSpeed, SummonColor }
+        };
+
+        public string Name => _damageClass.DisplayName.Value.TrimStart();
+        public Color Color => Colors.GetValueOrDefault(_damageClass, GenericColor);
+
+        public bool Equals(DamageClassDamageSource other)
+        {
+            return _damageClass == other._damageClass;
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (ReferenceEquals(null, obj)) return false;
+            if (ReferenceEquals(this, obj)) return true;
+            if (obj.GetType() != GetType()) return false;
+            return Equals((DamageClassDamageSource)obj);
+        }
+
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(_damageClass);
+        }
+    }
+
+    public class NPCDamageSource : IDamageSource
+    {
+        public static readonly NPCDamageSource The = new();
+        public string Name => "NPC damage";
+
+        private NPCDamageSource()
+        {
+        }
+    }
+
     public class UIDeathSummary(IEnumerable<Player.HurtInfo> damageTaken, int time) : UIState
     {
         public override void OnInitialize()
@@ -65,7 +126,7 @@ public class DeathSummaryManager : ModSystem
                 Height = { Percent = 0.05f }
             };
 
-            Dictionary<DamageClass, int> damageClassDamage = new();
+            Dictionary<IDamageSource, int> damageSources = new();
 
             var total = 0;
             foreach (var info in damageTaken)
@@ -73,14 +134,20 @@ public class DeathSummaryManager : ModSystem
                 if (info.DamageSource.SourceItem != null)
                 {
                     total += info.Damage;
-                    damageClassDamage[info.DamageSource.SourceItem.DamageType] =
-                        damageClassDamage.GetValueOrDefault(info.DamageSource.SourceItem.DamageType, 0) + info.Damage;
+                    var damageSource = new DamageClassDamageSource(info.DamageSource.SourceItem.DamageType);
+                    damageSources[damageSource] = damageSources.GetValueOrDefault(damageSource, 0) + info.Damage;
+                }
+                else if (info.DamageSource.SourceNPCIndex != -1)
+                {
+                    total += info.Damage;
+                    var damageSource = NPCDamageSource.The;
+                    damageSources[damageSource] = damageSources.GetValueOrDefault(damageSource, 0) + info.Damage;
                 }
             }
 
-            total = 150;
-            damageClassDamage[DamageClass.Magic] = 100;
-            damageClassDamage[DamageClass.Ranged] = 50;
+            total += 150;
+            damageSources[new DamageClassDamageSource(DamageClass.Magic)] = 100;
+            damageSources[new DamageClassDamageSource(DamageClass.Ranged)] = 50;
 
             // DamageClass[] damageClasses = [DamageClass.Magic, DamageClass.Ranged, DamageClass.Melee, DamageClass.Summon, DamageClass.Generic];
 
@@ -91,7 +158,7 @@ public class DeathSummaryManager : ModSystem
             };
             // FIXME: Fixed order for damage classes.
             var left = 0.0f;
-            foreach (var (damageClass, count) in damageClassDamage)
+            foreach (var (damageClass, count) in damageSources)
             {
                 var percent = (float)count / total;
                 var segment = new UISegment(damageClass, percent, count)
@@ -135,28 +202,11 @@ public class DeathSummaryManager : ModSystem
             };
         }
 
-        public class UISegment(DamageClass damageClass, float percent, int total) : UIPanel
+        public class UISegment(IDamageSource damageSource, float percent, int total) : UIPanel
         {
-            private static readonly Color DefaultColor = new(160, 160, 160);
-            private static readonly Color GenericColor = new(221, 80, 71);
-            private static readonly Color SummonColor = new(79, 203, 203);
-            private static readonly Color MeleeColor = new(219, 220, 49);
-
-            private static readonly Dictionary<DamageClass, Color> DamageClassColors = new()
-            {
-                { DamageClass.Default, DefaultColor },
-                { DamageClass.Generic, GenericColor },
-                { DamageClass.Melee, MeleeColor },
-                { DamageClass.MeleeNoSpeed, MeleeColor },
-                { DamageClass.Ranged, new(21, 194, 86) },
-                { DamageClass.Magic, new(204, 31, 198) },
-                { DamageClass.Summon, SummonColor },
-                { DamageClass.SummonMeleeSpeed, SummonColor }
-            };
-
             public override void OnInitialize()
             {
-                BackgroundColor = DamageClassColors.GetValueOrDefault(damageClass, GenericColor);
+                BackgroundColor = damageSource.Color;
             }
 
             public override void MouseOver(UIMouseEvent evt)
@@ -179,7 +229,7 @@ public class DeathSummaryManager : ModSystem
                     var text = Main.keyState.PressingShift() ? total.ToString() : $"{percent * 100:F0}%";
 
                     // FIXME: Multiple of these likely don't add up to 100%
-                    UICommon.TooltipMouseText($"{text}{damageClass.DisplayName}");
+                    UICommon.TooltipMouseText($"{text} {damageSource.Name}");
                 }
             }
         }
