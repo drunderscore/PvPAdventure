ecf51d25dc6b509571c3496d90665bd7923a400b
James Puleo <james@jame.xyz>
2025-09-02 21:58:13 -0400

    AdventureBossBar: Render the highest team in front of others

--- AdventureBossBar.cs
+++ AdventureBossBar.cs
@@ -1,3 +1,4 @@
+using System.Linq;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Terraria;
@@ -20,19 +21,34 @@ public class AdventureBossBar : GlobalBossBar
             Utils.CenteredRectangle(Main.ScreenSize.ToVector2() * new Vector2(0.5f, 1f) + new Vector2(0f, -50f),
                 bossBarPosition.ToVector2());
 
+        // FIXME: This should just be a sorted dictionary by value so we can render all of them in the correct Z order.
+        var highest = npc.GetGlobalNPC<AdventureNpc>().TeamLife.MinBy(v => v.Value);
+
         foreach (var (team, life) in npc.GetGlobalNPC<AdventureNpc>().TeamLife)
         {
             // Shouldn't be possible, but be sure not to draw this.
             if (team == Team.None)
                 continue;
 
+            // Don't draw the highest team just yet -- ensure it draws in front of everyone else.
+            if (team == highest.Key)
+                continue;
+
+            Draw(team, life);
+        }
+
+        Draw(highest.Key, highest.Value);
+
+        return;
+
+        void Draw(Team team, int life)
+        {
             var lifeRemaining = (float)life / npc.lifeMax;
 
             var frame = TextureAssets.Pvp[1].Value.Frame(6);
             frame.X = frame.Width * (int)team;
 
-            // FIXME: looks silly when overlapping, because alpha colors blend...
-            //              maybe stack/stagger them? that takes math.
+            // FIXME: Looks a little silly when overlapping, maybe they should get stacked or staggered when on top.
             var color = Color.White;
             color *= lifeRemaining.Remap(0.85f, 1.0f, 1.0f, 0.0f);
 
