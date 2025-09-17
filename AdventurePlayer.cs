2863f5cd4ca1c9e839d1e925a97369e151b2e91e
EJCrispy <ej@soundstasty.com>
2025-09-16 17:21:27 -0700

    You now have max luck (double) at shimmer

--- AdventurePlayer.cs
+++ AdventurePlayer.cs
@@ -1228,6 +1228,17 @@ public class NewIchorPlayer : ModPlayer
     }
 }
 
+public class AetherLuckPlayer : ModPlayer
+{
+    public override void ModifyLuck(ref float luck)
+    {
+        if (Player.ZoneShimmer)
+        {
+            luck += 400; // 400 times the amount of normal luck
+        }
+    }
+}
+
 public class ShinyStoneHotswap : ModBuff
 {
     public override string Texture => $"PvPAdventure/Assets/Buff/ShinyStoneHotswap";
