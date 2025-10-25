9565f464f9b7f42f203c2d4d5d3965c5c75ec175
emyhrberg <erik.myhrberg@hotmail.com>
2025-10-25 05:16:57 +0200

    Added magic mirror countdown

---
+++ Core/Features/MagicMirrorCombatText/CustomMirrorCombatText.cs
@@ -0,0 +1,20 @@
+﻿//using Terraria;
+//using Terraria.ID;
+//using Terraria.ModLoader;
+
+//namespace PvPAdventure.Core.Features.MagicMirrorCombatText
+//{
+//    internal class CustomMirrorCombatText : GlobalItem
+//    {
+//        public override bool? UseItem(Item item, Player player)
+//        {
+//            if (item.type == ItemID.MagicMirror)
+//            {
+//                player.GetModPlayer<MagicMirrorPlayer>().usedMirror = true;
+//                return false;
+//            }
+
+//            return base.UseItem(item, player);
+//        }
+//    }
+//}
---
+++ Core/Features/MagicMirrorCombatText/MagicMirrorPlayer.cs
@@ -0,0 +1,44 @@
+﻿using Terraria;
+using Terraria.ModLoader;
+using Microsoft.Xna.Framework;
+
+namespace PvPAdventure.Core.Features.MagicMirrorCombatText
+{
+    public class MagicMirrorPlayer : ModPlayer
+    {
+        //private TimeSpan timeSinceLastText = TimeSpan.Zero;
+        //private readonly TimeSpan interval = TimeSpan.FromSeconds(1); // 1 second interval
+        //public bool usedMirror = false;
+
+        public override void PostUpdate()
+        {
+            Item currentItem = Main.LocalPlayer.HeldItem;
+
+            bool playerIsUsingMagicMirror = currentItem != null && 
+                currentItem.type == Terraria.ID.ItemID.MagicMirror && 
+                Main.LocalPlayer.itemAnimation > 0;
+
+            if (playerIsUsingMagicMirror && Main.LocalPlayer.itemAnimation % 60 == 0)
+            {
+                int itemUseTimeLeft = Main.LocalPlayer.itemAnimation / 60; // Convert to seconds
+                CombatText.NewText(Player.getRect(), Color.Cyan, itemUseTimeLeft); // Display over the player
+            }
+
+            //if (!usedMirror) return; 
+
+            //// Increment timer
+            //timeSinceLastText += TimeSpan.FromSeconds(1.0 / 60.0);
+
+            //if (timeSinceLastText >= interval)
+            //{
+            //    timeSinceLastText = TimeSpan.Zero;
+
+            //    if (Player.whoAmI == Main.myPlayer)
+            //    {
+            //    }
+
+            //    usedMirror = false; // Reset flag after showing text once
+            //}
+        }
+    }
+}
