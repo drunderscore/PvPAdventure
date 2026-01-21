9aecbba71f746362865f532c6f6f12d17a7ac853
EJCrispy <ej@soundstasty.com>
2026-01-21 08:33:04 -0800

    Added features from missinginmaster into erkys features
    - Added RecipeManager from MissinginMaster
    - Added the ShimmerRecipeGroups from MissinginMaster
    - Added the twins logic from MissinginMaster as well
    - Correctly file scoped the BiomeKeyMold namespaces
    - Added summaries
    - Changed biome keys to CamelCase
    - Changed to file-based namespaces

---
+++ Common/Items/BiomeKeyReplacer.cs
@@ -0,0 +1,51 @@
+﻿using PvPAdventure.Content.Items.BiomeKeyMolds;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Terraria;
+using Terraria.DataStructures;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace PvPAdventure.Common.Items;
+/// <summary>
+/// -  Checks when a list of relelvant "Biome Key" items are spawned using OnSpawn <br/>
+/// -  Replaces relevant "Biome Key" items if they are "dropped" by a non-player <br/>
+/// - They are replaced with our ModItems, the BiomeKeyMold counterparts <br/>
+/// - This emulates pre-1.3 behavior, where enemies dropped biome key molds instead of the keys outright <br/>
+/// </summary>
+public class BiomeKeyReplacer : GlobalItem
+{
+    public override void OnSpawn(Item item, IEntitySource source)
+    {
+        switch (item.type)
+        {
+            case ItemID.CorruptionKey:
+                item.SetDefaults(ModContent.ItemType<CorruptionKeyMold>());
+                break;
+            case ItemID.HallowedKey:
+                item.SetDefaults(ModContent.ItemType<HallowedKeyMold>());
+                break;
+            case ItemID.FrozenKey:
+                item.SetDefaults(ModContent.ItemType<FrozenKeyMold>());
+                break;
+            case ItemID.JungleKey:
+                item.SetDefaults(ModContent.ItemType<JungleKeyMold>());
+                break;
+            case ItemID.DungeonDesertKey:
+                item.SetDefaults(ModContent.ItemType<DesertKeyMold>());
+                break;
+        }
+
+        if (item.type == ModContent.ItemType<CorruptionKeyMold>() ||
+            item.type == ModContent.ItemType<HallowedKeyMold>() ||
+            item.type == ModContent.ItemType<FrozenKeyMold>() ||
+            item.type == ModContent.ItemType<JungleKeyMold>() ||
+            item.type == ModContent.ItemType<DesertKeyMold>())
+        {
+            item.stack = 1;
+        }
+    }
+}
---
+++ Content/Items/BiomeKeyMolds/DesertKeyMold.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace PvPAdventure.Content.Items.BiomeKeyMolds;
+/// <summary>
+/// An item that is just used for crafting
+/// It was at one point a vanilla item, but has since been removed
+/// Recipe can be found in RecipeManager
+/// </summary>
+
+public class DesertKeyMold : ModItem
+{
+    public override string Texture => $"PvPAdventure/Assets/Items/DesertKeyMold";
+
+    public override void SetStaticDefaults()
+    {
+    }
+
+    public override void SetDefaults()
+    {
+        Item.maxStack = 9999;
+        Item.rare = ItemRarityID.Yellow;
+    }
+}
