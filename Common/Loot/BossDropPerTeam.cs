129f4a202f7d27a0fcb7f07be18e453d11a1ad6e
Erky <121192176+emyhrberg@users.noreply.github.com>
2026-01-30 06:54:55 +0100

    Disable vanilla boss bag purple swirl color
    Allows for clearer treasure bag outlines. Massive detour though; not great for longevity.

--- Common/Loot/BossDropPerTeam.cs
+++ Common/Loot/BossDropPerTeam.cs
@@ -2,15 +2,18 @@
 using Microsoft.Xna.Framework.Graphics;
 using PvPAdventure.Common.UI.WorldItems;
 using PvPAdventure.Core.Config;
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Reflection;
 using Terraria;
 using Terraria.DataStructures;
 using Terraria.Enums;
+using Terraria.GameContent;
 using Terraria.GameContent.ItemDropRules;
 using Terraria.ID;
 using Terraria.ModLoader;
+using Terraria.UI;
 
 namespace PvPAdventure.Common.Loot;
 
@@ -20,12 +23,188 @@ internal sealed class BossDropPerTeamSystem : ModSystem
     {
         On_CommonCode.DropItem_Rectangle_IEntitySource_int_int_bool += DropItem_TagBossLoot;
         On_CommonCode.DropItemLocalPerClientAndSetNPCMoneyTo0 += DropBossBag_TeamOnly;
+        On_Main.DrawItem += DisableBossBagGlow;
     }
 
     public override void Unload()
     {
         On_CommonCode.DropItem_Rectangle_IEntitySource_int_int_bool -= DropItem_TagBossLoot;
         On_CommonCode.DropItemLocalPerClientAndSetNPCMoneyTo0 -= DropBossBag_TeamOnly;
+        On_Main.DrawItem -= DisableBossBagGlow;
+    }
+
+    private static void DisableBossBagGlow(On_Main.orig_DrawItem orig, Main self, Item item, int whoami)
+    {
+        if (!item.active || item.IsAir)
+        {
+            return;
+        }
+        Main.instance.LoadItem(item.type);
+        Main.instance.DrawItem_GetBasics(item, whoami, out var texture, out var frame, out var glowmaskFrame);
+        Vector2 vector = frame.Size() / 2f;
+        Vector2 vector2 = new Vector2((float)(item.width / 2) - vector.X, item.height - frame.Height);
+        Vector2 vector3 = item.position - Main.screenPosition + vector + vector2;
+        float num = item.velocity.X * 0.2f;
+        if (item.shimmered)
+        {
+            num = 0f;
+        }
+        float scale = 1f;
+        Color color = Lighting.GetColor(item.Center.ToTileCoordinates());
+        Color currentColor = item.GetAlpha(color);
+        if (item.shimmered)
+        {
+            currentColor.R = (byte)(255f * (1f - item.shimmerTime));
+            currentColor.G = (byte)(255f * (1f - item.shimmerTime));
+            currentColor.B = (byte)(255f * (1f - item.shimmerTime));
+            currentColor.A = (byte)(255f * (1f - item.shimmerTime));
+        }
+        else if (item.shimmerTime > 0f)
+        {
+            currentColor.R = (byte)((float)(int)currentColor.R * (1f - item.shimmerTime));
+            currentColor.G = (byte)((float)(int)currentColor.G * (1f - item.shimmerTime));
+            currentColor.B = (byte)((float)(int)currentColor.B * (1f - item.shimmerTime));
+            currentColor.A = (byte)((float)(int)currentColor.A * (1f - item.shimmerTime));
+        }
+        ItemSlot.GetItemLight(ref currentColor, ref scale, item);
+        if (ItemLoader.PreDrawInWorld(item, Main.spriteBatch, color, currentColor, ref num, ref scale, whoami))
+        {
+            int num2 = item.glowMask;
+            if (!Main.gamePaused && (item.IsACoin || item.type == 58 || item.type == 109) && color.R > 60 && (float)Main.rand.Next(500) - (Math.Abs(item.velocity.X) + Math.Abs(item.velocity.Y)) * 10f < (float)(color.R / 50))
+            {
+                int type = 43;
+                Color newColor = Color.White;
+                int alpha = 254;
+                float scale2 = 0.5f;
+                if (item.IsACoin)
+                {
+                    newColor = default(Color);
+                    alpha = 0;
+                    scale2 = 1f;
+                }
+                switch (item.type)
+                {
+                    case 71:
+                        type = 244;
+                        break;
+                    case 72:
+                        type = 245;
+                        break;
+                    case 73:
+                        type = 246;
+                        break;
+                    case 74:
+                        type = 247;
+                        break;
+                }
+                int num3 = Dust.NewDust(item.position, item.width, item.height, type, 0f, 0f, alpha, newColor, scale2);
+                Main.dust[num3].velocity *= 0f;
+            }
+            if (ItemID.Sets.BossBag[item.type])
+            {
+                float num4 = (float)item.timeSinceItemSpawned / 240f + Main.GlobalTimeWrappedHourly * 0.04f;
+                float globalTimeWrappedHourly = Main.GlobalTimeWrappedHourly;
+                globalTimeWrappedHourly %= 4f;
+                globalTimeWrappedHourly /= 2f;
+                if (globalTimeWrappedHourly >= 1f)
+                {
+                    globalTimeWrappedHourly = 2f - globalTimeWrappedHourly;
+                }
+                globalTimeWrappedHourly = globalTimeWrappedHourly * 0.5f + 0.5f;
+                for (float num5 = 0f; num5 < 1f; num5 += 0.25f)
+                {
+                    //Main.spriteBatch.Draw(texture, vector3 + new Vector2(0f, 8f).RotatedBy((num5 + num4) * ((float)Math.PI * 2f)) * globalTimeWrappedHourly, frame, new Color(90, 70, 255, 50), num, vector, scale, SpriteEffects.None, 0f);
+                }
+                for (float num6 = 0f; num6 < 1f; num6 += 0.34f)
+                {
+                    //Main.spriteBatch.Draw(texture, vector3 + new Vector2(0f, 4f).RotatedBy((num6 + num4) * ((float)Math.PI * 2f)) * globalTimeWrappedHourly, frame, new Color(140, 120, 255, 77), num, vector, scale, SpriteEffects.None, 0f);
+                }
+            }
+            else if (item.type == 75)
+            {
+                float num7 = (float)item.timeSinceItemSpawned / 240f + Main.GlobalTimeWrappedHourly * 0.04f;
+                float globalTimeWrappedHourly2 = Main.GlobalTimeWrappedHourly;
+                globalTimeWrappedHourly2 %= 5f;
+                globalTimeWrappedHourly2 /= 2.5f;
+                if (globalTimeWrappedHourly2 >= 1f)
+                {
+                    globalTimeWrappedHourly2 = 2f - globalTimeWrappedHourly2;
+                }
+                globalTimeWrappedHourly2 = globalTimeWrappedHourly2 * 0.5f + 0.5f;
+                for (float num8 = 0f; num8 < 1f; num8 += 0.25f)
+                {
+                    Main.spriteBatch.Draw(TextureAssets.Item[item.type].Value, vector3 + new Vector2(0f, 8f).RotatedBy((num8 + num7) * ((float)Math.PI * 2f)) * globalTimeWrappedHourly2, frame, new Color(50, 50, 255, 50), num, vector, scale, SpriteEffects.None, 0f);
+                }
+                for (float num9 = 0f; num9 < 1f; num9 += 0.34f)
+                {
+                    Main.spriteBatch.Draw(TextureAssets.Item[item.type].Value, vector3 + new Vector2(0f, 4f).RotatedBy((num9 + num7) * ((float)Math.PI * 2f)) * globalTimeWrappedHourly2, frame, new Color(120, 120, 255, 127), num, vector, scale, SpriteEffects.None, 0f);
+                }
+            }
+            else if (item.type == 4143)
+            {
+                float num10 = (float)item.timeSinceItemSpawned / 240f + Main.GlobalTimeWrappedHourly * 0.04f;
+                float globalTimeWrappedHourly3 = Main.GlobalTimeWrappedHourly;
+                globalTimeWrappedHourly3 %= 5f;
+                globalTimeWrappedHourly3 /= 2.5f;
+                if (globalTimeWrappedHourly3 >= 1f)
+                {
+                    globalTimeWrappedHourly3 = 2f - globalTimeWrappedHourly3;
+                }
+                globalTimeWrappedHourly3 = globalTimeWrappedHourly3 * 0.5f + 0.5f;
+                for (float num11 = 0f; num11 < 1f; num11 += 0.34f)
+                {
+                    Main.spriteBatch.Draw(TextureAssets.Item[item.type].Value, vector3 + new Vector2(0f, 8f).RotatedBy((num11 + num10) * ((float)Math.PI * 2f)) * globalTimeWrappedHourly3, frame, new Color(30, 30, 155, 60), num, vector, scale, SpriteEffects.None, 0f);
+                }
+                for (float num12 = 0f; num12 < 1f; num12 += 0.34f)
+                {
+                    Main.spriteBatch.Draw(TextureAssets.Item[item.type].Value, vector3 + new Vector2(0f, 4f).RotatedBy((num12 + num10) * ((float)Math.PI * 2f)) * globalTimeWrappedHourly3, frame, new Color(60, 60, 127, 57), num, vector, scale, SpriteEffects.None, 0f);
+                }
+                Main.spriteBatch.Draw(texture, vector3, frame, new Color(255, 255, 255, 128), num, vector, scale, SpriteEffects.None, 0f);
+            }
+            if ((item.type >= 1522 && item.type <= 1527) || item.type == 3643)
+            {
+                currentColor = ((!(item.shimmerTime > 0f)) ? new Color(250, 250, 250, Main.mouseTextColor / 2) : new Color((int)(250f * (1f - item.shimmerTime)), (int)(250f * (1f - item.shimmerTime)), (int)(250f * (1f - item.shimmerTime)), (int)((float)(Main.mouseTextColor / 2) * (1f - item.shimmerTime))));
+                scale = (float)(int)Main.mouseTextColor / 1000f + 0.8f;
+            }
+            if (item.type == 3779)
+            {
+                num2 = -1;
+            }
+            Main.spriteBatch.Draw(texture, vector3, frame, currentColor, num, vector, scale, SpriteEffects.None, 0f);
+            if (item.shimmered)
+            {
+                Main.spriteBatch.Draw(texture, vector3, frame, new Color(currentColor.R, currentColor.G, currentColor.B, 0), num, vector, scale, SpriteEffects.None, 0f);
+            }
+            if (item.color != Color.Transparent)
+            {
+                Main.spriteBatch.Draw(texture, vector3, frame, item.GetColor(color), num, vector, scale, SpriteEffects.None, 0f);
+            }
+            if (num2 != -1)
+            {
+                Color color2 = new Color(250, 250, 250, item.alpha);
+                if (item.type == 5146)
+                {
+                    color2 = new Color(Main.DiscoR, Main.DiscoG, Main.DiscoB);
+                }
+                Main.spriteBatch.Draw(TextureAssets.GlowMask[num2].Value, vector3, frame, color2, num, vector, scale, SpriteEffects.None, 0f);
+            }
+            if (ItemID.Sets.TrapSigned[item.type])
+            {
+                Main.spriteBatch.Draw(TextureAssets.Wire.Value, vector3 + frame.Size().RotatedBy(num) * 0.45f * item.scale, new Rectangle(4, 58, 8, 8), currentColor, 0f, new Vector2(4f), 1f, SpriteEffects.None, 0f);
+            }
+            if (item.type == 3858)
+            {
+                Main.spriteBatch.Draw(TextureAssets.GlowMask[233].Value, vector3, glowmaskFrame, new Color(255, 255, 255, 63) * 0.75f, num, glowmaskFrame.Size() / 2f, scale, SpriteEffects.None, 0f);
+            }
+            if (ItemID.Sets.DrawUnsafeIndicator[item.type])
+            {
+                Vector2 vector4 = new Vector2(-4f, -4f) * scale;
+                Texture2D value = TextureAssets.Extra[258].Value;
+                Rectangle rectangle = value.Frame();
+                Main.spriteBatch.Draw(value, vector3 + vector4 + frame.Size().RotatedBy(num) * 0.45f * item.scale, rectangle, currentColor, num, rectangle.Size() / 2f, 1f, SpriteEffects.None, 0f);
+            }
+        }
+        ItemLoader.PostDrawInWorld(item, Main.spriteBatch, color, currentColor, num, scale, whoami);
     }
 
     private static Team GetAwardTeam(NPC npc)
@@ -180,9 +359,14 @@ public sealed class BossDropPerTeamGlobalItem : GlobalItem
         //Color border = Main.teamColor[(int)team.Value];
         border.A = 255;
 
-        //DrawWorldItemOutline(item, sb, lightColor, alphaColor, rotation, scale, border);
+        DrawWorldItemOutline(item, sb, lightColor, alphaColor, rotation, scale, border);
 
         return base.PreDrawInWorld(item, sb, lightColor, alphaColor, ref rotation, ref scale, whoAmI);
+    }
+
+    public override void Update(Item item, ref float gravity, ref float maxFallSpeed)
+    {
+        //base.Update(item, ref gravity, ref maxFallSpeed);
     }
 
     private void DrawWorldItemOutline(Item item, SpriteBatch sb, Color lightColor, Color alphaColor, float rotation, float scale, Color borderColor)
