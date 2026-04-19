753f54234860d1ce7b9f8df233f726b90d86cb9c
EJCrispy <ej@soundstasty.com>
2026-04-19 01:24:47 -0700

    Did as erky requested to the best of my ability
    fixed the unintentional gitignore
    removed fixed respawn time
    Added back accidentally removed comments from beetlearmorplayer.cs

--- Common/Combat/PvP/PvPBeetleArmorPlayer.cs
+++ Common/Combat/PvP/PvPBeetleArmorPlayer.cs
@@ -22,12 +22,15 @@ internal class PvPBeetleArmorPlayer : ModPlayer
 
     public override void Load()
     {
+        // Remove logic for handling Beetle Might buffs.
         IL_Player.UpdateBuffs += EditPlayerUpdateBuffs;
+        // Simplify logic for handling Beetle Scale Mail set bonus to do the bare minimum required.
         IL_Player.UpdateArmorSets += EditPlayerUpdateArmorSets;
     }
 
     public override void SetStaticDefaults()
     {
+        // Beetle Might buffs last forever until death.
         BuffID.Sets.TimeLeftDoesNotDecrease[BuffID.BeetleMight1] = true;
         BuffID.Sets.TimeLeftDoesNotDecrease[BuffID.BeetleMight2] = true;
         BuffID.Sets.TimeLeftDoesNotDecrease[BuffID.BeetleMight3] = true;
@@ -35,6 +38,7 @@ internal class PvPBeetleArmorPlayer : ModPlayer
 
     public override void PostUpdateEquips()
     {
+        /// we apply the glowing eye effect from Yoraiz0rsSpell item
         if (Player.HasBuff(BuffID.BeetleMight3))
             Player.yoraiz0rEye = 33;
     }
@@ -130,12 +134,17 @@ internal class PvPBeetleArmorPlayer : ModPlayer
         var cursor = new ILCursor(il);
 
         ILLabel label = null;
+        // First, find a load of Player.buffType that is somewhere followed by a load of BuffID.BeetleMight1 and a blt
+        // instruction...
         cursor.GotoNext(i =>
             i.MatchLdfld<Player>("buffType") && i.Next.Next.Next.MatchLdcI4(98) &&
             i.Next.Next.Next.Next.MatchBlt(out label));
 
+        // ...and go back to the "this" load...
         cursor.Index -= 1;
+        // ...while ensuring that instructions removed and emitted are labeled correctly...
         cursor.MoveAfterLabels();
+        // ...to emit a branch to the fail case.
         cursor.EmitBr(label);
     }
 
@@ -143,18 +152,23 @@ internal class PvPBeetleArmorPlayer : ModPlayer
     {
         var cursor = new ILCursor(il);
 
+        // Find a load to a string...
         cursor.GotoNext(i => i.MatchLdstr("ArmorSetBonus.BeetleDamage"));
+        // ...and go back to the branch instruction...
         cursor.Index -= 4;
 
+        // ...to grab it's label...
         var label = (ILLabel)cursor.Next!.Operand;
         cursor.Index += 1;
 
+        // ...and prepare a delegate call, doing the bare minimum for set bonus functionality...
         cursor.EmitLdarg0();
         cursor.EmitDelegate((Player self) =>
         {
             self.setBonus = Language.GetTextValue("ArmorSetBonus.BeetleDamage");
             self.beetleOffense = true;
         });
+        // ...then branch away so we skip the original code.
         cursor.EmitBr(label);
     }
 }
