0d544099b63521b730a33948a7380b3f768b87a2
Erky <121192176+emyhrberg@users.noreply.github.com>
2026-07-22 17:38:11 +0200

    Fix server config crash
    Stackoverflow from ModConfig.WrapIt , ErkySSC ConfigSystem?

--- Core/Config/ConfigElements/ShopItemListElement.cs
+++ Core/Config/ConfigElements/ShopItemListElement.cs
@@ -1,9 +1,12 @@
 using System;
 using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
 using System.Reflection;
 using Terraria;
 using Terraria.ID;
 using Terraria.Localization;
+using Terraria.ModLoader.Config;
 using Terraria.ModLoader.Config.UI;
 using Terraria.ModLoader.UI;
 using Terraria.UI;
@@ -12,6 +15,41 @@ namespace PvPAdventure.Core.Config.ConfigElements;
 
 internal sealed class ShopItemListElement : ListElement
 {
+    private PropertyFieldWrapper valueMember;
+    private Type wrapperType;
+
+    // Wrap each list slot in a plain property that does not carry ShopItems' CustomModConfigItem
+    // attribute. Passing the original ShopItems member back to WrapIt recursively constructs this
+    // same ShopItemListElement until the process exhausts its stack.
+    private sealed class EntryWrapper<T>
+    {
+        private readonly IList list;
+        private readonly int index;
+
+        public EntryWrapper(IList list, int index)
+        {
+            this.list = list;
+            this.index = index;
+        }
+
+        [Expand(false)]
+        public T Value
+        {
+            get => index >= 0 && index < list.Count ? (T)list[index] : default;
+            set
+            {
+                if (index >= 0 && index < list.Count)
+                    list[index] = value;
+            }
+        }
+    }
+
+    public override void OnBind()
+    {
+        base.OnBind();
+        EnsureValueMember();
+    }
+
     protected override void SetupList()
     {
         DataList.Clear();
@@ -20,20 +58,20 @@ internal sealed class ShopItemListElement : ListElement
         if (Data is not IList items)
             return;
 
-        Type itemType = MemberInfo.Type.GetGenericArguments()[0];
+        EnsureValueMember();
+
         for (int i = 0; i < items.Count; i++)
         {
             int index = i;
-            ServerConfig.ShopItem shopItem = items[index] as ServerConfig.ShopItem;
-            Tuple<UIElement, UIElement> wrapped = UIModConfig.WrapIt(
-                DataList, ref top, MemberInfo, Item, 0, Data, itemType, index);
+            object wrapper = Activator.CreateInstance(wrapperType, items, index);
+            Tuple<UIElement, UIElement> wrapped = UIModConfig.WrapIt(DataList, ref top, valueMember, wrapper, index);
 
             wrapped.Item2.Left.Pixels += 24f;
             wrapped.Item2.Width.Pixels -= 30f;
             Collapse(wrapped.Item2);
 
             if (wrapped.Item2 is ConfigElement element)
-                element.TextDisplayFunction = () => $"{index + 1}: {Display(shopItem)}";
+                element.TextDisplayFunction = () => $"{index + 1}: {Display(index < items.Count ? items[index] as ServerConfig.ShopItem : null)}";
 
             UIModConfigHoverImage delete = new(
                 DeleteTexture,
@@ -50,6 +88,20 @@ internal sealed class ShopItemListElement : ListElement
             };
             wrapped.Item1.Append(delete);
         }
+    }
+
+    private void EnsureValueMember()
+    {
+        if (valueMember != null)
+            return;
+
+        Type itemType = listType ?? MemberInfo.Type.GetGenericArguments()[0];
+        wrapperType = typeof(EntryWrapper<>).MakeGenericType(itemType);
+
+        IList dummyList = (IList)Activator.CreateInstance(typeof(List<>).MakeGenericType(itemType));
+        object dummyWrapper = Activator.CreateInstance(wrapperType, dummyList, 0);
+        valueMember = ConfigManager.GetFieldsAndProperties(dummyWrapper)
+            .First(member => member.Name == nameof(EntryWrapper<object>.Value));
     }
 
     private static string Display(ServerConfig.ShopItem entry)
